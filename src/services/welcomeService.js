7977072757c2c089a38d9456f6b12e6eb5a3d61c
Ally4 <el.ally741@gmail.com>
2022-02-26 09:40:48 +0200

    another

--- src/services/welcomeService.js
+++ src/services/welcomeService.js
@@ -4,7 +4,7 @@ import { successfullResponse, failureResponse, serverErrorResponse } from '../ut
 class Welcome {
 static async signin(req, res, next) {
   try{
-  return successfullResponse(res, 200, 'Welcome to our wonderfull system!.' )
+  return successfullResponse(res, 200, 'Welcome to our wonderfull system!., this is another version of it' )
 }catch (error) {
   return serverErrorResponse(res, 500, `The is the error from the server ${error}`)
 }
